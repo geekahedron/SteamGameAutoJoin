a901ca5e82a0f33bfaed59204214fcfbbbf0f692
geekahedron <d_j_@hotmail.com>
2015-06-17 14:57:19 -0400

    Update autojoin.user.js

--- autojoin.user.js
+++ autojoin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name	Steam Game AutoJoin
 // @namespace	https://github.com/geekahedron/SteamGameAutoJoin/
-// @version	1.0a
+// @version	1.1a
 // @description	Auto-join script for 2015 Summer Steam Monster Minigame
 // @author	geekahedron
 // @match	*://steamcommunity.com/minigame
