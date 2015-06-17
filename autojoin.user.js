213941d4acd07f96c1cc8bb6b5c7c18a7b1e8a57
geekahedron <d_j_@hotmail.com>
2015-06-17 13:02:38 -0400

    Update autojoin.user.js

--- autojoin.user.js
+++ autojoin.user.js
@@ -15,13 +15,13 @@
 
 function addGlobalStyle(css)
 {
-	var head, style;
-	head = document.getElementsByTagName('head')[0];
-	if (!head) { return; }
-	style = document.createElement('style');
-	style.type = 'text/css';
-	style.innerHTML = css;
-	head.appendChild(style);
+	var sgaj_head, sgaj_style;
+	sgaj_head = document.getElementsByTagName('head')[0];
+	if (!sgaj_head) { return; }
+	sgaj_style = document.createElement('style');
+	sgaj_style.type = 'text/css';
+	sgaj_style.innerHTML = css;
+	head.appendChild(sgaj_style);
 }
 
 function GetCurrentGame()
@@ -38,33 +38,33 @@ function DisplayUI()
 		var current = GetCurrentGame();
 		play_div.children[0].children[0].children[0].innerHTML = "Resume Your Game (" + current + ")";
 	}
-	var sp = document.createElement("span");
-	sp.innerHTML = '<a onClick="javascript:AutoJoinGame()" class="main_btn"><span>Auto Join Game<span></a><input type=text id="autojoinid" name="autojoinid" class="main_btn" />';
-	game_div.appendChild(sp,game_div.children[0]);
+	var sgaj_sp = document.createElement("span");
+	sgaj_sp.innerHTML = '<a onClick="javascript:AutoJoinGame()" class="main_btn"><span>Auto Join Game<span></a><input type=text id="autojoinid" name="autojoinid" class="main_btn" />';
+	game_div.appendChild(sgaj_sp,game_div.children[0]);
 	addGlobalStyle('.section_play .current_game, .section_play .new_game {  margin-top: 10px; }');
 	
 }
 
 function CheckAndLeaveCurrentGame( callback )
 {
-	var currentgame = GetCurrentGame();
-	console.log('Current Game: ' + currentgame);
+	var sgaj_currentgame = GetCurrentGame();
+	console.log('Current Game: ' + sgaj_currentgame);
 
-	if (currentgame == 0)
+	if (sgaj_currentgame == 0)
 		return callback();
 
 	$J.post(
 		'http://steamcommunity.com/minigame/ajaxleavegame/',
-		{ 'gameid' : currentgame, 'sessionid' : g_sessionID }
+		{ 'gameid' : sgaj_currentgame, 'sessionid' : g_sessionID }
 	).done( function() { callback(); }
 	);
 }
 
 function AutoJoinGame(gameID)
 {
-	var gameID = document.getElementById("autojoinid").value;
+	var sgaj_gameID = document.getElementById("autojoinid").value;
 	CheckAndLeaveCurrentGame( function() {
-		JoinGameID_Real( gameID );
+		JoinGameID_Real( sgaj_gameID );
 	});
 }
 
